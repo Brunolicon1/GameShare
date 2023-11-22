fc7d226f4403b34834c9c08d9d01b82315acb196
Bruno Licon <brunolicon1@gmail.com>
2023-11-22 17:27:47 -0300

    alterações finais 2.0

--- login/loguin.js
+++ login/loguin.js
@@ -1,7 +1,11 @@
+//cria uma contante que recebe o parametro do id password do html
 const password = document.getElementById('password')
+
+//cria uma constante que recebe o paramentro do id icon do html
 const icon = document.getElementById('icon')
 
-function showHide(){ //conexão com html
+//função que altera o tipo do atributo conforme o click do usuario
+function showHide(){ 
     if(password.type === 'password'){ 
         password.setAttribute('type', 'text') //mostrar
         icon.classList.add('hide')
--- script.js
+++ script.js
@@ -1,16 +1,21 @@
+//inicia uma variavel de contagem
 let count = 1;
+
+//inicializa quando o primeiro estiver selecionado
 document.getElementById("radio1").checked = true
 
+//cria uma função com intervalo de 5000 milisegundos (5segundos)
 setInterval(function(){
    nextImage(); 
 },5000)
 
+//função que vai para a proxima imagem 
 function nextImage() {
     count ++;
     if(count>4){
         count = 1;
     }
-    
+//exibe conforme o contador 
     document.getElementById("radio" +count).checked = true
 }
 
